68efb9b19a1121bb797aaa8f528829ece936105b
jyp <bbie6772@daum.net>
2026-08-28 23:51:51 +0900

    feat(player): 플레이어 이동 및 대쉬 구현
    Player 의 내부 rigidbody와 Input 을 읽어와 이동을 구현,
    Physics2D.Raycast 를 이용해 충돌 판정(벽) 이후 대쉬(순간이동)이 되도록 구현

---
+++ Assets/_Project/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed = 1.0f;
+
+    [SerializeField] private float _dashDistance = 5.0f;
+    private Vector2 _movementInput; // 이동 입력 저장, Update에서 프레임별 입력 FixedUpdate에서 일정 주기 이동 적용
+    private Rigidbody2D _rigidbody; // 장착된 rigidbody 참조하기
+
+
+    [SerializeField] private LayerMask _wallLayer;
+    [SerializeField] private float _wallOffset = 0.5f;
+    [SerializeField] private float _dashCooldown = 1.0f;
+    private float _dashCooldownRemaining = 0.0f;
+    private bool _dashInput = false;
+    
+    // 초기화에 사용되는 메서드
+    void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 방향키 입력 하드코딩 > 나중에 리매핑하기 어려운 점 이해하기 (Input패키지가 최근방식임)
+        _movementInput.x = (Input.GetKey(KeyCode.D) ? 1 : 0) - (Input.GetKey(KeyCode.A) ? 1 : 0);
+        _movementInput.y = (Input.GetKey(KeyCode.W) ? 1 : 0) - (Input.GetKey(KeyCode.S) ? 1 : 0);
+
+        // 대쉬 입력
+        _dashInput = _dashInput || Input.GetKeyDown(KeyCode.Space);
+   
+        // 대각선 방향으로 이동 시에 빨라지는 부분 수정, 입력값을 아날로그로 받으면 정규화가 강제로 1로 키울 수 있음
+        _movementInput.Normalize();
+    }
+
+    // 엔진이 프레임 간격을 재고, 0.02초마다 업데이트 하는 메서드
+    void FixedUpdate()
+    {
+        // 이동 업데이트
+        _rigidbody.linearVelocity = _movementInput * _moveSpeed;
+
+        // 대쉬 쿨타임 적용
+        if (_dashCooldownRemaining >= 0.0f)
+        {
+            _dashCooldownRemaining -= Time.fixedDeltaTime;
+        }
+        
+        // 대쉬 사용
+        if (_dashInput)
+        {
+            if (_dashCooldownRemaining <= 0.0f)
+            {
+                RaycastHit2D hit = Physics2D.Raycast(_rigidbody.position, _movementInput, _dashDistance, _wallLayer);
+                Vector2 position = _rigidbody.position;
+                if (hit.collider != null)
+                {
+                    position = hit.point + (hit.normal * _wallOffset);
+                }
+                else
+                {
+                    position = _rigidbody.position + (_movementInput * _dashDistance);
+                }
+                
+                _rigidbody.position = position;
+                _dashCooldownRemaining = _dashCooldown;
+            }
+            _dashInput = false;
+        }
+    }
+}
